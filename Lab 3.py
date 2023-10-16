2a76226c39929e5ddb42923b572e7f723c31f32d
TheGrandCircus <scottj0910@students.bowiestate.edu>
2023-10-16 11:07:51 -0700

    BUIS-305 Class Labs

---
+++ Lab 3.py
@@ -0,0 +1,26 @@
+import math
+
+number1= input('Enter number 1 ')
+number2= input('Enter number 2 ')
+print(number1,number2)
+sum=number1+number2
+print(sum)
+multiplication =int(number1)*int(number2)
+print(multiplication)
+print(type(multiplication))
+difference= int(number1)-int(number2)
+multiplication =int(number1)*int(number2)
+division=int(number1)/int(number2)
+print(difference, multiplication, division)
+
+#Return the value of 9 raised to the power of 3
+print(math.pow(7,4))
+print(math.degrees(5))
+print(math.cos(6))
+print(math.exp(75))
+print(math.degrees(95))
+print(math.radians(78))
+print(math.sin(23))
+print(math.pi)
+print(math.tau)
+print(math.e)
