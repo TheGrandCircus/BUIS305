0952235b74762bd24a7d2bca77d7a10d1f2cde12
TheGrandCircus <scottj0910@students.bowiestate.edu>
2023-11-01 09:10:30 -0700

    BUIS-305 Class Labs

---
+++ Question 3 Midterm.py
@@ -0,0 +1,11 @@
+Num1 = float(input("Enter first number: "))
+Num2 = float(input("Enter second number: "))
+Num3 = float(input("Enter third number: "))
+Num4 = float(input("Enter fourth number: "))
+Num5 = float(input("Enter fifth number:"))
+
+sumofnumbers = Num1 + Num2 + Num3 + Num4 + Num5
+average = sumofnumbers/5
+
+print("The Sum of the glorious numbers that you selected is %.2f"%sumofnumbers)
+print("The Average of the glorious numbers that you selected is %.2f"%average)
